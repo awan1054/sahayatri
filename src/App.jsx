18d3fa0ad59bd47a7c0a19e982a873b20988bb95
awan1054 <awankarki1054@gmail.com>
2024-09-26 21:41:57 +0545

    maila

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
+import Hero from './components/hero'
 
 function App() {
   return (
     <div>
      <Navbar/>
      <Banner/>
+    <Hero/>
+
     </div>
   )
 }
